2f0d0edd5d3552b09d7d4eadadd6b8239772af64
Artem Petrov <Artyom1350@gmail.com>
2023-04-06 17:04:15 +0500

    deleted:    EDMSystemTest/src/app/users/user/classes/applClass.ts 	new file:   EDMSystemTest/src/app/users/user/classes/inApplClass.ts 	new file:   EDMSystemTest/src/app/users/user/classes/outApplClass.ts

---
+++ EDMSystemTest/src/app/users/user/classes/inApplClass.ts
@@ -0,0 +1,8 @@
+export class IncomeApplClass{
+    id: number;
+    user_id:number;
+    title: string;
+    description: string;
+    dateAppl: Date;
+    created_at: Date;
+}
---
+++ EDMSystemTest/src/app/users/user/classes/outApplClass.ts
@@ -0,0 +1,7 @@
+export class OutAppl{
+    id: number;
+    title: string;
+    description: string;
+    dateAppl: Date;
+    created_at: Date;
+}
